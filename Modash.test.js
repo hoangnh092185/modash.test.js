4de31bb5fd40e41fc4a02ab3ad5586867c18d89c
Nhat Hoang <hoangnh092185@gmail.com>
2017-03-29 11:45:31 -0700

    Test using Jest.

--- Modash.test.js
+++ Modash.test.js
@@ -1,43 +1,39 @@
 import Modash from './Modash';
 
-function assertEqual(description, actual, expected) {
-  if(actual === expected){
-    console.log(`[PASS] ${description}`);
-  } else {
-    console.log(`[FAIL] ${description}`);
-    console.log(`\tactual: '${actual}'`);
-    console.log(`\texpected: '${expected}'`);
-  }
-}
+describe('Modash', () => {
+  describe ('`truncate()`', () => {
+    const string = 'there was one catch, and that was CATCH-22';
 
-let actual;
-let expected;
-let string;
-
-string = 'there was one catch, and that was CATCH-22';
-actual = Modash.truncate(string, 19);
-expected = 'there was one catch...';
-
-assertEqual('`truncate()`: truncates a string', actual, expected);
-
-actual = Modash.truncate(string, string.length);
-expected = string;
-
-assertEqual('`truncate()`: no-ops if <= lenght', actual, expected);
-
-actual = Modash.capitalize(string);
-expected= 'There was one catch, and that was catch-22';
-
-assertEqual(`'capitalize()'; capitalizes the string`, actual, expected);
-
-string = 'customer responded at';
-actual = Modash.camelCase(string);
-expected = 'customerRespondedAt';
-
-assertEqual('`camelCase()`: string with spaces', actual, expected);
-
-string = 'customer_responded_at';
-actual = Modash.camelCase(string);
-expected = 'customerRespondedAt';
-
-assertEqual('`camelCase()`: string with underscore', actual, expected);
+    it('`truncate()`: truncates a string', () => {
+      expect(
+        Modash.truncate(string, 19)
+      ).toEqual('there was one catch...');
+    });
+    it('no-ops if <= lenght', () => {
+      expect(
+        Modash.truncate(string,string.length)
+      ).toEqual(string);
+    });
+  });
+  describe('`capitalize()`', () => {
+    it('capitalize first letter, lowercases rest', () => {
+      const string = 'there was one catch, and that was CATCH-22';;
+      expect(Modash.capitalize(string)
+    ).toEqual('There was one catch, and that was catch-22');
+    });
+  });
+  describe('`camelCase`', () => {
+    it('camelize string with spaces', () => {
+      const string = 'customer responded at';
+      expect(
+        Modash.camelCase(string)
+      ).toEqual('customerRespondedAt');
+    });
+    it('camelize string with underscores', () => {
+      const string = 'customer_responded_at';
+      expect(
+        Modash.camelCase(string)
+      ).toEqual('customerRespondedAt');
+    });
+  });
+});
